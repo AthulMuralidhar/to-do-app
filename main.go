49d3e0c9dc11fb5f1c7e1979ddcb683b8b7d1454
Athul Muralidhar <athul.muralidhar@bricr.com>
2021-11-17 10:57:45 +0100

    first commit with working index page

---
+++ main.go
@@ -0,0 +1,26 @@
+// original sauce: https://dev.to/matijakrajnik/1-starting-gin-server-g5k
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	router := gin.Default()
+
+	api := router.Group("/api")
+	{
+		api.GET("/", func(ctx *gin.Context) {
+			ctx.JSON(200, gin.H{"msg": "index page"})
+		})
+	}
+
+	router.NoRoute(func(ctx *gin.Context) {
+		ctx.JSON(http.StatusNotFound, gin.H{})
+	})
+
+	// router at http://localhost:8000/api/
+	router.Run(":8000")
+
+}
